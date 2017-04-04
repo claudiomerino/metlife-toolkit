aafe18debc9212747474ecdb2c3c4585b1fbf18d
Jair <ejairn@gmail.com>
2017-04-04 09:44:57 -0500

    Update cards, buttons, fonts, footer and navs

--- src/assets/toolkit/scripts/components/card.js
+++ src/assets/toolkit/scripts/components/card.js
@@ -1,6 +1,8 @@
 
 	let $openContent = $( '.openContent' );
 	let $toggleContent = $( '.toggleContent' ).data( 'toggle-content' );
+	let $restText = $( '.openContentText' ).data( 'rest-text' );
+	let $openText = $( '.openContentText' ).data( 'open-text' );
 
 	$openContent.on( 'click', ( e ) => {
 		e.preventDefault();
@@ -10,16 +12,32 @@
 		let $openContentData = $( e.currentTarget ).data( 'open-content' );
 		let $toggleContentData = $( '.toggleContent[data-toggle-content=' + $openContentData + ']' );
 		let $openContentText = $( e.currentTarget ).find( 'span' );
+		let $dataClose = $( e.currentTarget ).find( 'span' ).data( 'close' );
 
 		$toggleContentData.toggleClass( 'is-open' );
 		$openContentText.toggleClass( 'is-open' );
 		$( e.currentTarget ).toggleClass( 'is-open' );
 
-		if( $openContentText.hasClass( 'is-open' ) ) {
-			$openContentText.html( 'Mostrar menos información' );
+		if( $dataClose ) {
+
+			if( !$openContentText.hasClass( 'is-open' ) ) {
+				$openContentText.html( $restText );
+			}
+			else {
+				$openContentText.html( $openText );
+			}
+
 		}
+
 		else {
-			$openContentText.html( 'Mostrar más información' );
+
+			if( $openContentText.hasClass( 'is-open' ) ) {
+				$openContentText.html( $restText );
+			}
+			else {
+				$openContentText.html( $openText );
+			}
+
 		}
 
 	});
